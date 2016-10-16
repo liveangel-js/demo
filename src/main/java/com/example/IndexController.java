6541bb951bc3519e113bb9d40179cc1633bde4dc
liveangel <jiangshu.social@gmail.com>
2016-10-16 23:21:12 +0800

    add index.html; change IndexController to /i

--- src/main/java/com/example/IndexController.java
+++ src/main/java/com/example/IndexController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class IndexController {
-    @RequestMapping("/")
+    @RequestMapping("/i")
     public String index() {
         return "Greetings from Spring Boot!";
     }
