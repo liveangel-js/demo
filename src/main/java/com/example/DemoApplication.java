b44a39d3e1efe24730de7d09899eb7f5fe0f7062
liveangel <jiangshu.social@gmail.com>
2016-10-20 21:33:55 +0800

    move user auth to db; auto load admin user jsyu33; default register as user

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,18 +1,28 @@
 package com.example;
 
 import com.example.dao.CustomerRepository;
+import com.example.dao.RoleRepository;
+import com.example.dao.UserRepository;
 import com.example.model.Customer;
+import com.example.model.Role;
+import com.example.model.User;
+import com.example.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class DemoApplication {
 	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
@@ -28,7 +38,8 @@ public class DemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(CustomerRepository repository) {
+	public CommandLineRunner demo(CustomerRepository repository, RoleRepository roleRepository , UserRepository userRepository, @Autowired
+    UserService userService) {
 		return (args) -> {
 			// save a couple of customers
 			repository.save(new Customer("Jack", "Bauer", "Jack Bauer", "jb@gmail.com", "Nanjing"));
@@ -61,6 +72,15 @@ public class DemoApplication {
 				log.info(bauer.toString());
 			}
 			log.info("");
+
+            roleRepository.save(new Role("User"));
+            roleRepository.save(new Role("Admin"));
+            Role role = roleRepository.findByName("Admin");
+            User admin = new User("jsyu32","jiangshu1992");
+            Set<Role> roles = new HashSet<>();
+            roles.add(role);
+            admin.setRoles(roles);
+            userService.save(admin);
 		};
 	}
 }
--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -3,20 +3,68 @@ package com.example.model;
 /**
  * Created by liveangel on 2016-10-16.
  */
+
+import javax.persistence.*;
+import java.util.Set;
+
+
+@Entity
+@Table(name = "user")
 public class User {
-    private final long id;
-    private final String userName;
-
-    public User(long id, String userName) {
-        this.id = id;
-        this.userName = userName;
+    private Long id;
+    private String username;
+    private String password;
+    private String passwordConfirm;
+    private Set<Role> roles;
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
     }
+    public User(){
 
-    public long getId() {
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
         return id;
     }
 
-    public String getUserName() {
-        return userName;
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Transient
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
