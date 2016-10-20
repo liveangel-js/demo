238fc04af920d0358973766a552a76542b119f1e
liveangel <jiangshu.social@gmail.com>
2016-10-20 16:48:18 +0800

    change slogin to login

--- src/main/java/com/example/LoginController.java
+++ src/main/java/com/example/LoginController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-    @RequestMapping("/slogin")
+    @RequestMapping("/login")
     public String index() {
-        return "slogin";
+        return "login";
     }
 }
